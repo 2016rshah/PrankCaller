e747cfe7710ea3577c03084b9f60a2a479def5d0
Rushi Shah <2016rshah@gmail.com>
2014-10-23 21:43:34 +0000

    Removed template code

--- prankcaller/client/client.js
+++ prankcaller/client/client.js
@@ -1,21 +1,4 @@
 if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault("counter", 0);
-    
-    Template.hello.helpers({
-        counter: function () {
-          return Session.get("counter");
-        }
-    });
-    
-    Template.hello.events({
-        'click button': function () {
-          // increment the counter when button is clicked
-          Session.set("counter", Session.get("counter") + 1);
-          //Meteor.call("sendCall", "Add a number here that you want to send the call to");
-        }
-    });
-    
     Template.sendCall.events({
         'submit form#callForm' : function(event, template){
             event.preventDefault();
