8eb6f65253e13a6987fd024494fc748b6691d73b
Rushi Shah <2016rshah@gmail.com>
2014-10-24 01:54:40 +0000

    Lots and lots of database stuff

--- prankcaller/server/server.js
+++ prankcaller/server/server.js
@@ -1,5 +1,5 @@
 if (Meteor.isServer) {
-	//Add twilio code from sticky note here with key and stuff
+	//twilio from sticky note here
 	Meteor.startup(function () {
 	// // code to run on server at startup
 	// Meteor.http.get("https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4", {}, function(err, data){
@@ -17,22 +17,42 @@ if (Meteor.isServer) {
 	Meteor.methods({
 		sendText : function(toNum, bodyText){
 			//add number to database here
-			twilio.sendSms({
-			    to:'+1'+toNum, // Any number Twilio can deliver to
-			    from: '+19718034372', // A number you bought from Twilio and can use for outbound communication
-			    body: bodyText // body of the SMS message
-			  }, function(err, responseData) { //this function is executed when a response is received from Twilio
-			    if (!err) { // "err" is an error received during the request, if any
-			      // "responseData" is a JavaScript object containing data received from Twilio.
-			      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-			      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-			      console.log(responseData.from); // outputs "+14506667788"
-			      console.log(responseData.body); // outputs "word to your mother."
-			    }
+			
+			
+			var boundFunction = Meteor.bindEnvironment(function(err, responseData){
+				if (!err) { // "err" is an error received during the request, if any
+					// "responseData" is a JavaScript object containing data received from Twilio.
+					// A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+					// http://www.twilio.com/docs/api/rest/sending-sms#example-1
+					console.log(responseData.from); // outputs "+14506667788"
+					console.log(responseData.body); // outputs "word to your mother."
+					if(texted.find({"number":toNum}).fetch().length === 0){
+						texted.insert({"number":toNum, "content":[bodyText]});  
+					}
+					else{
+						texted.update({"number":toNum}, {$push : {"content": bodyText}});
+					}
+				}
+			},
+			function(e){
+				throw e;
 			});
+			
+			
+			twilio.sendSms({
+					to:'+1'+toNum, // Any number Twilio can deliver to
+					from: '+19718034372', // A number you bought from Twilio and can use for outbound communication
+					body: bodyText // body of the SMS message
+				}, function(err, responseData){boundFunction(err, responseData)});
 			
 		}, 
 		sendCall : function(toNum){
+			if(called.find({"number":toNum}).fetch().length === 0){
+				called.insert({"number":toNum, "frequency":1});  
+			}
+			else{
+				called.update({"number":toNum}, {$inc : {"frequency": 1}});
+			}
 			twilio.makeCall(
 				{
 					to:'+1'+toNum, // Any number Twilio can call
@@ -42,6 +62,9 @@ if (Meteor.isServer) {
 				function(err, responseData) {
 					//executed when the call has been initiated.
 					console.log(responseData.from); // outputs "+14506667788"
+					if(called.find({"number":toNum}).fetch().length === 0){
+						called.insert({"number":toNum});
+					}
 				}
 			);
 		}
