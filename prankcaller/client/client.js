f8c3a8fef63bcf6d8e0afdc8ecf7ea42acec2593
Rushi Shah <2016rshah@gmail.com>
2014-10-26 01:45:24 +0000

    Pass the soundbyte url to server

--- prankcaller/client/client.js
+++ prankcaller/client/client.js
@@ -1,10 +1,18 @@
 if (Meteor.isClient) {
     Template.sendCall.events({
         'submit form#callForm' : function(event, template){
+            var soundBytes = [
+                "http://twimlets.com/echo?Twiml=%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%20%3F%3E%3CResponse%3E%3CPlay%3Ehttp%3A%2F%2Fwww.shockwave-sound.com%2Fsound-effects%2Fhalloween-sounds%2FEvil%2520Witch%2520Laughing.mp3%3C%2FPlay%3E%3C%2FResponse%3E",
+                "http://twimlets.com/echo?Twiml=%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%20%3F%3E%3CResponse%3E%3CPlay%3Ehttp%3A%2F%2Fwww.shockwave-sound.com%2Fsound-effects%2Fhalloween-sounds%2FEvil%2520Laugh%2520In%2520Hall.mp3%3C%2FPlay%3E%3C%2FResponse%3E",
+                "http://twimlets.com/echo?Twiml=%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%20%3F%3E%3CResponse%3E%3CPlay%3Ehttp%3A%2F%2Fwww.shockwave-sound.com%2Fsound-effects%2Fhalloween-sounds%2Fthunderbolt.mp3%3C%2FPlay%3E%3C%2FResponse%3E",
+                
+            ];
+            var index = $("input[type='radio'][name='sound']:checked").val();
             event.preventDefault();
             var number = template.find("#callNumber").value;
             console.log(number);
-            Meteor.call("sendCall", number);
+            
+            Meteor.call("sendCall", number, soundBytes[index]);
         }
     });
     Template.sendText.events({
